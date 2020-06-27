74e9cea8dd00dc137956c542a58dddbfdbec1de1
TeodorDyakov <tedy1996@gmail.com>
2020-06-27 07:15:35 -0700

    add script from kontrolno 2

--- scripts/K2_2016_SI_2.sh
+++ scripts/K2_2016_SI_2.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 1 -o $(id -u) -ne 0 ]; then
 	exit 1
 fi
 
 users=$(ps -eo user | tail -n +2 | sort | uniq)
 
-echo $users
+total_rss=0
 
 for user in $users; do
 	
 	while read line; do
 		echo $line
-	done< <(ps -u $user -o pid,rss) 
+		rss=$(echo $line | awk '{print $2}') 
+		total_rss=$(($total_rss + $rss))
+	done< <(ps -u $user -o pid,rss | tail -n +2)
+		
+	if [ $total_rss -gt $1 ]; then
+		max_rss_pid=$(ps -u $user -o pid,rss | tail -n +2 | sort -n -k2 | awk 			'{print $1'})
+		kill -s SIGTERM $max_rss_pid
+		sleep 2		
+		kill -s SIGKILL $max_rss_pid		
+	fi 
 done
