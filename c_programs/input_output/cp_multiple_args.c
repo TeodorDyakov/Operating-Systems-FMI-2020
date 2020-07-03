417344dac376296e982f152e76c9f2d96f4949e0
Teo <tedy1996@gmail.com>
2020-07-03 16:36:59 +0300

    make the string coying safe using strncpy, add tasks statements

--- c_programs/input_output/cp_multiple_args.c
+++ c_programs/input_output/cp_multiple_args.c
@@ -19,23 +19,26 @@ int main (int argc, char* argv[])
 		if(fd == -1){
 				err(99, "failed to open file");					
 		}
+			
+		const size_t path_max_length = 255;
 		
-		char c;
-		
-		char path[255];
-
-		strcpy(path, argv[argc - 1]);
-		strcat(path, "/"); 
-		strcat(path, argv[i]);
+		char path[path_max_length + 1];
+		path[path_max_length] = '\0';
+	
+		strncpy(path, argv[argc - 1], path_max_length);
+		strncat(path, "/", path_max_length - strlen(path) - 1); 
+		strncat(path, argv[i], path_max_length - strlen(path) - strlen(argv[i]));
 	
 		int copy_fd = open(path, O_CREAT | O_WRONLY);
 				
 		if(copy_fd == -1){
 				err(99, "failed to open file");					
 		}
-
+		
+		char c;
+		
 		while(read(fd, &c, 1)){
-			write(dir_fd, &c, 1);		
+			write(copy_fd, &c, 1);		
 		}
 			
 	}
