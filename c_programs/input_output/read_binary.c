972427c619fec371f3216fdd5a2da632b56e65f2
Teo <tedy1996@gmail.com>
2020-07-07 17:11:24 +0300

    solution of last tasks, now complete and tested

--- c_programs/input_output/read_binary.c
+++ c_programs/input_output/read_binary.c
@@ -3,7 +3,8 @@
 #include<fcntl.h>
 #include<stdlib.h>
 #include<string.h>
-#include<ctype.h>
+#include<stdint.h>
+#include<err.h>
 
 int is_little_endian(){
 	unsigned int i = 1;  
@@ -15,17 +16,18 @@ int is_little_endian(){
 }
 
 int main(int argc, char** argv){
-	if(argc != 2){
+	if(argc != 3){
 		errx(1, "err");	
 	}
-			
-	int fd = open(argv[1], O_RDONLY);  
-	if(fd == -1){
-		err(2, "%s", argv[1]);
-	}
 	
-	int min = 0;
-	int max = 0;
+	int fd = open(argv[2], O_RDONLY);  
+	
+	if(fd == -1){
+		err(2, "%s", argv[2]);
+	}
+		
+	uint16_t min = INT16_MAX;
+	uint16_t max = 0;
 	
 	int read_cnt = 0;
 	char buf[2];
@@ -35,7 +37,7 @@ int main(int argc, char** argv){
 			//this means we have reached EOF so we stop reading
 			break;
 		}
-		u_int16_t a = 0;
+		uint16_t  a = 0;
 		if(is_little_endian()){
 			//swap byte order if on little endian arch
 			char swapped[2] = {buf[1], buf[0]};
@@ -43,13 +45,30 @@ int main(int argc, char** argv){
 			cast the address of swapped to a pointer
 			to unsigned 16 bit int then dereference it 
 			*/
-			a = *(u_int16_t*)(&swapped);		
-		}else{
-			a = *(u_int16_t*)(&buf);
-		}						
-		printf("%d\n", a); 				
+			a = *(uint16_t*)(&swapped);		
+		} else {
+			a = *(uint16_t *)(&buf);
+		}
+		
+		if(strcmp(argv[1], "--min") == 0){
+			if(min > a){
+				min = a;
+			}		 		
+		} else if(strcmp(argv[1], "--max") == 0){
+			if(max < a){
+				max = a;
+			}
+		} else { 						
+			printf("%d\n", a);
+		} 				
 	}
-	
+		
+	if(strcmp(argv[1], "--min") == 0){
+		printf("min is %d", min);		 		
+	} else if(strcmp(argv[1], "--max") == 0){
+		printf("max is %d", max);
+	}
+
 	close(fd);
 	exit(0);  
 }  
