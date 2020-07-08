04fd23170ec61c84fd9393e7c9f91e183c6740b9
Teo <tedy1996@gmail.com>
2020-07-08 20:26:18 +0300

    add error hadling

--- c_programs/processes/task4.c
+++ c_programs/processes/task4.c
@@ -3,25 +3,48 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<err.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdio.h>
 
 int main(int argc, char** argv){
-	int fd = open("temp", O_CREAT | O_WRONLY, S_IRUSR, S_IWUSR);
+	int fd = open("temp", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+	if(fd == -1){
+		err(1, "%s", "temp");
+	}
 	
 	pid_t pid  = fork();
 	
 	if(pid == -1){
+		close(fd);
+		perror("fork");
 		exit(-1);
-	}else if(pid > 0){
-		wait(NULL);
+	} else if(pid > 0){
+		if (wait(NULL) == -1){
+			close(fd);
+			perror("wait");
+			exit(-2);
+		}
 		
 		pid = fork();
-		if(pid > 0){
-			write(fd, "bar", 3);
+
+		if(pid == -1){
+			close(fd);
+			perror("fork");
+			exit(-3);
+		} else if(pid > 0){
+			if(write(fd, "bar", 3) != 3){
+				close(fd);
+				errx(1, "error in writing to file");
+			}
 		}
 	}else{
-		write(fd, "foo", 3);
+		if(write(fd, "foo", 3) != 3){
+			close(fd);
+			errx(1, "error in writing to file");
+		}
 	}
+	
 	close(fd);
+	exit(0);
 }
