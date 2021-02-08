d4a7d79fe7b0b23437024fd843e847cc79a395a8
TeodorDyakov <tedy1996@gmail.com>
2021-02-08 19:39:24 +0200

    improve example

--- c_programs/text_vs_binary.c
+++ c_programs/text_vs_binary.c
@@ -1,45 +1,59 @@
 #include<unistd.h>
 #include<stdio.h>
 #include<fcntl.h>
+#include<string.h>
 #include<stdio.h>
+#include<stdlib.h>
 
-//Use thi program so you can learn abou text vs binary
-//use a hex editor so you can inspect the the files binary.bin and text.txt
+//Use this program so you can learn about text vs binary representation of data
+//use a hex editor so you can inspect the files binary.bin and text.txt
 
 char buf[8];
-char str[8];
+char id_str[8];
+
+struct Student{
+	char name[16];
+	int id;
+};
 
 int main(int argc, char ** argv){
 	
+	//lets save the struct as binary
+	struct Student student;
+	student.id = 0xabcd;
+	strcpy(student.name, "Teodor");
+
 	int fd = open("binary.bin", O_WRONLY | O_CREAT, 0666);
-
-	const int num = 0xabcd;
-
-	write(fd, &num, sizeof(num));
+	write(fd, &student, sizeof(student));
 	close(fd);
-
+	//ghex binary.bin and inspect the file
+	//is the file human readable? which parts are and which are not? What if the name was in Cyrilic?
+	//look at the the byte order for the id - is it big endian or little endian?
+	
+	//now lets the file read it back to student struct
 	fd = open("binary.bin", O_RDONLY);
-	int rd = 0;
-	read(fd, &rd, sizeof(rd));
+	read(fd, &student, sizeof(student));
 
-	printf("Number read from binary.bin file as int: %d\n", rd);
+	printf("name: %s\nid: %d\n", student.name, student.id);
 	close(fd);
 
-	sprintf(str, "%d", num);
+	//now lets save the student struct as readable text	
 	fd = open("text.txt", O_RDWR | O_CREAT, 0666);
-	write(fd, str, sizeof(str));
+	write(fd, &student.name, strlen(student.name));
+	write(fd, ",", 1);
+
+	//convert the int idf to string
+	sprintf(id_str, "%d", student.id);
+	write(fd, &id_str, strlen(id_str));
 	close(fd);
 
+	//now lets read it back into student struct 
 	fd = open("text.txt", O_RDONLY);
-	rd = 0;
-	read(fd, &rd, sizeof(rd));
-	printf("Number read from text.txt as int: %d\n", rd);
-	close(fd);
+	read(fd, &student.name, strlen(student.name));
+	char c;
+	read(fd, &c, 1);
+	read(fd, &buf, strlen(id_str));
+	student.id = atoi(id_str);
+	printf("name: %s\nid: %d\n", student.name, student.id);
 
-	fd = open("text.txt", O_RDONLY);
-
-	read(fd, &buf, sizeof(buf));
-
-	printf("Number read from text.txt as text: %s\n", buf);
-	close(fd);
 }
